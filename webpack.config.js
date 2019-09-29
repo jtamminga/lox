25494903b285adfe9d85146a40105b59b880c0e1
jtamminga <john.h.tamminga@gmail.com>
2019-09-29 12:00:12 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+
+module.exports = {
+    mode: 'development',
+    // Change to your "entry-point".
+    entry: './src/lox',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'lox.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.json']
+    },
+    module: {
+        rules: [{
+            // Include ts, tsx, js, and jsx files.
+            test: /\.(ts|js)x?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+        }],
+    }
+};
